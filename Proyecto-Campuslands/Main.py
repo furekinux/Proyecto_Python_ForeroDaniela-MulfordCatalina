5de6289b8062d3eacac286a499d5bef5857ae88e
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-02-10 13:40:46 -0500

    Main.py

--- Proyecto-Campuslands/Main.py
+++ Proyecto-Campuslands/Main.py
@@ -1,1 +1,17 @@
 ##Primer archivo de prueba
+import json
+con = open("DATA/campers.json")
+campersJS = json.load(con)
+
+conec = open("DATA/gente.json")
+genteJS = json.load(conec)
+
+conecta = open("DATA/notas.json")
+notasJS = json.load(conecta)
+
+conlos = open("DATA/Roles.json")
+rolesJS = json.load(conlos)
+
+conjsons = open("DATA/salas.json")
+salasJS = json.load(conjsons)
+
