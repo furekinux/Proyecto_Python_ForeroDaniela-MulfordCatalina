7f8a73733f6e4a7b9d43d062ef93a6e2b9b80313
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-02-13 14:32:27 -0500

    Update main.py

--- Proyecto-Campuslands/main.py
+++ Proyecto-Campuslands/main.py
@@ -69,7 +69,7 @@ if quien == "Trainer" or quien == "trainer":
     
     ##-----------------------Inicio de coordinación--------------------------##
 elif quien=="Coordinacion" or quien=="coordinacion":
-    hacer_cordi=int(input("Selecciona un número: \n 1. Editar/ver notas\n 2. Estado \n 3. Crear rutas\n 4. Asignar\n"))
+    hacer_cordi=int(input("Selecciona un número: \n 1. Editar/ver notas\n 2. Estado \n 3. Crear rutas\n 4. Asignar\n 5. Listar"))
     
     if hacer_cordi==1: ##EDITAR/VER
         menu_notas=input("Deseas:\n 1. Ver notas salon.\n 2. Ver notas estudiante.\n 3. Poner nota.\n 4. Quitar nota.\n")
